174e0bd6f7e8afd2c14bfa8badfa50419f584fdb
AMLing2 <mathias.lervik@hotmail.com>
2024-11-17 14:36:33 +0100

    kyria keymap update

--- keyboards/splitkb/kyria/keymaps/deftwo/keymap.c
+++ keyboards/splitkb/kyria/keymaps/deftwo/keymap.c
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_RAISE] = LAYOUT(
       KC_TILD, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                     KC_6   , KC_7   , KC_8 ,   KC_9  ,  KC_0   , KC_DEL,
       _______, KC_LGUI, KC_LALT, KC_MUTE, KC_VOLU, KC_LBRC,                                     KC_RBRC, KC_PGUP, KC_UP  , KC_PGDN, KC_PSCR, KC_PIPE,
-      _______, KC_RCTL, KC_LSHT, _______, KC_VOLD, KC_LCBR, KC_INS , KC_SCRL, KC_RETN, KC_DEL , KC_RCBR, KC_LEFT, KC_DOWN, KC_RGHT, KC_END , KC_HOME,
+      _______, KC_RCTL, KC_LSFT, _______, KC_VOLD, KC_LCBR, KC_INS , KC_SCRL, KC_RETN, KC_DEL , KC_RCBR, KC_LEFT, KC_DOWN, KC_RGHT, KC_END , KC_HOME,
                                  _______, _______, _______, _______, _______, KC_RETN, KC_DEL , ADJUST , KC_RGUI, _______
     ),
 
@@ -124,9 +124,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_LOWER] = LAYOUT(
       KC_GRV ,  KC_EXLM ,KC_AT ,  KC_HASH,KC_DLR, KC_PERC,                                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_ESC ,
-      _______,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 , KC_F5  ,                                     KC_F6  , KC_UNDS, KC_EQL , KC_LCBR, KC_RCUI, KC_BSLS,
+      _______,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 , KC_F5  ,                                     KC_F6  , KC_UNDS, KC_EQL , KC_LCBR, KC_RCBR, KC_BSLS,
       KC_CAPS,  KC_F7 ,  KC_F8 ,  KC_F9 ,  KC_F1 , KC_F11 , _______, _______, _______, _______, KC_F12 , KC_MINS, KC_PLUS, KC_PIPE, _______, _______,
-                                 _______, ADJUST , _______, KC_DEL , _______, _______, _______, _______, _______, _______
+                                 _______, _______, ADJUST , KC_DEL , _______, _______, _______, _______, _______, _______
     ),
 
 /*
@@ -192,7 +192,7 @@ bool oled_task_user(void) {
         // clang-format on
 
         oled_write_P(qmk_logo, false);
-        oled_write_P(PSTR("Kyria rev1.0\n\n"), false);
+        oled_write_P(PSTR("Kyria rev1.4\n\n"), false);
 
         // Host Keyboard Layer Status
         oled_write_P(PSTR("Layer: "), false);
--- keyboards/splitkb/kyria/keymaps/deftwo/rules.mk
+++ keyboards/splitkb/kyria/keymaps/deftwo/rules.mk
@@ -1,4 +1,7 @@
 OLED_ENABLE = yes
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+ENCODER_RESOLUTION = 2       # Enables the use of one or more encoders
 RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+#COMMENT OUT FOR NON-pro micro clone
+BOOTLOADER = caterina
